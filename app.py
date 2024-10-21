2ae4a2b3a6fd537db2963e55d2aebf65d7c7afce
João Gabriel Novais de Araujo Petrola <92691038+Joopetrola@users.noreply.github.com>
2024-10-21 15:38:01 -0300

    app.py

---
+++ app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+def main():
+    st.set_page_config(
+        page_title="Classifica Folhas de Videira",
+        page_icon="��",
+    )
+
+    st.write("# Classifica Folhas de Videira! ��")
+    #Carrega modelo
+    #Carrega imagem
+    #Classifica
+
+if __name__ == "__main__":
+    main()
